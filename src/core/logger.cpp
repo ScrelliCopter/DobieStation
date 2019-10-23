bc89632af709b175e67858438ff29501446b3364
a dinosaur <nick@a-dinosaur.com>
2019-10-23 13:19:34 +1100

    colourise date & category stuff

--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -43,7 +43,7 @@ constexpr const char* level_esc(Level lvl)
     case (TRACE):    return ESC(DIM);
     case (DEBUG):    return ESC(FG_BLUE);
     //case (INFO):     return ESC(RESET);
-    case (WARN):     return ESC(FG_GREEN);
+    case (WARN):     return ESC(FG_YELLOW);
     case (ERROR):    return ESC(FG_RED);
     case (CRITICAL): return ESC2(BRIGHT, BG_RED);
 
@@ -55,32 +55,32 @@ constexpr const char* category_token(Category cat)
 {
     switch (cat)
     {
-    case (CAT_EE):         return "[EE] ";
-    case (CAT_EE_TIMING):  return "[EE Timing] ";
-    case (CAT_IOP):        return "[IOP] ";
-    case (CAT_IOP_DMA):    return "[IOP DMA] ";
-    case (CAT_IOP_TIMING): return "[IOP Timing] ";
-    case (CAT_COP0):       return "[COP0] ";
-    case (CAT_COP2):       return "[COP2] ";
-    case (CAT_FPU):        return "[FPU] ";
-    case (CAT_IPU):        return "[IPU] ";
-    case (CAT_CDVD):       return "[CDVD] ";
-    case (CAT_PAD):        return "[PAD] ";
-    case (CAT_SPU):        return "[SPU] ";
-    case (CAT_GIF):        return "[GIF] ";
-    case (CAT_GS):         return "[GS] ";
-    case (CAT_GS_R):       return "[GS_r] ";
-    case (CAT_GS_T):       return "[GS_t] ";
-    case (CAT_GS_JIT):     return "[GS JIT] ";
-    case (CAT_DMAC):       return "[DMAC] ";
-    case (CAT_SIO2):       return "[SIO2] ";
-    case (CAT_SIF):        return "[SIF] ";
-    case (CAT_VIF):        return "[VIF] ";
-    case (CAT_VU):         return "[VU] ";
-    case (CAT_VU0):        return "[VU0] ";
-    case (CAT_VU1):        return "[VU1] ";
-    case (CAT_VU_JIT):     return "[VU_JIT] ";
-    case (CAT_VU_JIT64):   return "[VU_JIT64] ";
+    case (CAT_EE):         return "EE: ";
+    case (CAT_EE_TIMING):  return "EE Timing: ";
+    case (CAT_IOP):        return "IOP: ";
+    case (CAT_IOP_DMA):    return "IOP DMA: ";
+    case (CAT_IOP_TIMING): return "IOP Timing: ";
+    case (CAT_COP0):       return "COP0: ";
+    case (CAT_COP2):       return "COP2: ";
+    case (CAT_FPU):        return "FPU: ";
+    case (CAT_IPU):        return "IPU: ";
+    case (CAT_CDVD):       return "CDVD: ";
+    case (CAT_PAD):        return "PAD: ";
+    case (CAT_SPU):        return "SPU: ";
+    case (CAT_GIF):        return "GIF: ";
+    case (CAT_GS):         return "GS: ";
+    case (CAT_GS_R):       return "GS_r: ";
+    case (CAT_GS_T):       return "GS_t: ";
+    case (CAT_GS_JIT):     return "GS JIT: ";
+    case (CAT_DMAC):       return "DMAC: ";
+    case (CAT_SIO2):       return "SIO2: ";
+    case (CAT_SIF):        return "SIF: ";
+    case (CAT_VIF):        return "VIF: ";
+    case (CAT_VU):         return "VU: ";
+    case (CAT_VU0):        return "VU0: ";
+    case (CAT_VU1):        return "VU1: ";
+    case (CAT_VU_JIT):     return "VU_JIT: ";
+    case (CAT_VU_JIT64):   return "VU_JIT64: ";
 
     default: return "";
     }
@@ -97,26 +97,30 @@ void write_date(FILE* f)
 
 void logger::log_write(Category cat, Level lvl, const std::string& str, char eol)
 {
-    fputs(level_esc(lvl), stderr);
-
     static Category last_cat = NUM_CATEGORIES;
     static char     last_eol = '\n';
 
     if (last_eol == '\n')
     {
+        fputs(ESC(DIM), stderr);
         write_date(stderr);
+        fputs(ESC(FG_GREEN), stderr);
         fputs(category_token(cat), stderr);
     }
     else if (cat != last_cat)
     {
         putc('\n', stderr);
+        fputs(ESC(DIM), stderr);
         write_date(stderr);
+        fputs(ESC(FG_GREEN), stderr);
         fputs(category_token(cat), stderr);
     }
 
     last_cat = cat;
     last_eol = eol;
 
+    fputs(ESC(RESET), stderr);
+    fputs(level_esc(lvl), stderr);
     fputs(str.c_str(), stderr);
     fputs(ESC(RESET), stderr);
     if (eol)
