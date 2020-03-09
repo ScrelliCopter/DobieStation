7b2fa0a54650066a64a367890a5e66a3c196ec9a
a dinosaur <nick@a-dinosaur.com>
2020-03-09 13:54:12 +1100

    move test_logger to loggingwindow

--- src/qt/loggingwindow.cpp
+++ src/qt/loggingwindow.cpp
@@ -7,6 +7,10 @@
 #include <QRadioButton>
 #include <QButtonGroup>
 
+using namespace logger;
+
+void test_logger();
+
 
 static constexpr std::array<std::tuple<logger::Category, const char*>, logger::NUM_CATEGORIES> cat_names = {
     std::make_tuple(logger::CAT_MISC, "Misc"),
@@ -75,4 +79,38 @@ LoggingWindow::LoggingWindow(QWidget* parent) :
     layout.setColumnStretch(0, 0);
     layout.setRowStretch(0, 0);
     setLayout(&layout);
+}
+
+
+void test_logger()
+{
+    info_l(CAT_MISC, "test {0}:{1:04X} {0}", "test", 10);
+    error(CAT_MISC, "poop {:i}", "oops"); // type error
+    const char text[24] = {'N', 'G', 'A', 'G', 'L', 'E', 'R', 'I', 'D', 'Y', 'M', 'O', 'T', 'H', 'F', 'U', 'C', 'K'};
+    critical_l(CAT_MISC, "{}{}{} {}{}{} {}{} {}{}",
+        fmt::basic_string_view<char>(text + 5, 1), fmt::basic_string_view<char>(text + 0, 4), fmt::basic_string_view<char>(text + 5, 1),
+        fmt::basic_string_view<char>(text + 6, 3), fmt::basic_string_view<char>(text + 4, 2), fmt::basic_string_view<char>(text + 9, 1),
+        fmt::basic_string_view<char>(text + 10, 4), fmt::basic_string_view<char>(text + 5, 2),
+        fmt::basic_string_view<char>(text + 14, 4), fmt::basic_string_view<char>(text + 5, 2));
+    info(CAT_MISC, "hi?");
+    warn(CAT_MISC, "hi!");
+    trace_l(CAT_EE, "hi.");
+
+    trace_l(CAT_MISC, "trace");
+    debug_l(CAT_MISC, "debug");
+    info_l(CAT_MISC, "info");
+    warn_l(CAT_MISC, "warn");
+    error_l(CAT_MISC, "error");
+    critical_l(CAT_MISC, "critical");
+
+    const char* cat_names[] = {"CAT_MISC", "CAT_EE", "CAT_EE_TIMING", "CAT_IOP", "CAT_IOP_DMA", "CAT_IOP_TIMING", "CAT_COP0", "CAT_COP2", "CAT_FPU", "CAT_IPU", "CAT_CDVD", "CAT_PAD", "CAT_SPU", "CAT_GIF", "CAT_GS", "CAT_GS_R", "CAT_GS_T", "CAT_GS_JIT", "CAT_DMAC", "CAT_SIO2", "CAT_VIF", "CAT_SIF", "CAT_VU", "CAT_VU0", "CAT_VU1", "CAT_VU_JIT", "CAT_VU_JIT64"};
+    for (int i = 0; i < NUM_CATEGORIES; ++i)
+    {
+        struct timespec wait = {0, 100000000};
+        nanosleep(&wait, &wait);
+        info(static_cast<Category>(i), "using category ");
+        warn(static_cast<Category>(i), cat_names[i]);
+        if (i == NUM_CATEGORIES - 1)
+            info_l(static_cast<Category>(i), "");
+    }
 }
--- src/qt/main.cpp
+++ src/qt/main.cpp
@@ -1,44 +1,7 @@
 #include <QApplication>
 #include <memory>
 #include "emuwindow.hpp"
-#include <core/logger.hpp>
 
-using namespace std;
-using namespace logger;
-
-
-void test_logger()
-{
-    info_l(CAT_MISC, "test {0}:{1:04X} {0}", "test", 10);
-    error(CAT_MISC, "poop {:i}", "oops"); // type error
-    const char text[24] = {'N', 'G', 'A', 'G', 'L', 'E', 'R', 'I', 'D', 'Y', 'M', 'O', 'T', 'H', 'F', 'U', 'C', 'K'};
-    critical_l(CAT_MISC, "{}{}{} {}{}{} {}{} {}{}",
-        fmt::basic_string_view<char>(text + 5, 1), fmt::basic_string_view<char>(text + 0, 4), fmt::basic_string_view<char>(text + 5, 1),
-        fmt::basic_string_view<char>(text + 6, 3), fmt::basic_string_view<char>(text + 4, 2), fmt::basic_string_view<char>(text + 9, 1),
-        fmt::basic_string_view<char>(text + 10, 4), fmt::basic_string_view<char>(text + 5, 2),
-        fmt::basic_string_view<char>(text + 14, 4), fmt::basic_string_view<char>(text + 5, 2));
-    info(CAT_MISC, "hi?");
-    warn(CAT_MISC, "hi!");
-    trace_l(CAT_EE, "hi.");
-
-    trace_l(CAT_MISC, "trace");
-    debug_l(CAT_MISC, "debug");
-    info_l(CAT_MISC, "info");
-    warn_l(CAT_MISC, "warn");
-    error_l(CAT_MISC, "error");
-    critical_l(CAT_MISC, "critical");
-
-    const char* cat_names[] = {"CAT_MISC", "CAT_EE", "CAT_EE_TIMING", "CAT_IOP", "CAT_IOP_DMA", "CAT_IOP_TIMING", "CAT_COP0", "CAT_COP2", "CAT_FPU", "CAT_IPU", "CAT_CDVD", "CAT_PAD", "CAT_SPU", "CAT_GIF", "CAT_GS", "CAT_GS_R", "CAT_GS_T", "CAT_GS_JIT", "CAT_DMAC", "CAT_SIO2", "CAT_VIF", "CAT_SIF", "CAT_VU", "CAT_VU0", "CAT_VU1", "CAT_VU_JIT", "CAT_VU_JIT64"};
-    for (int i = 0; i < NUM_CATEGORIES; ++i)
-    {
-        struct timespec wait = {0, 100000000};
-        nanosleep(&wait, &wait);
-        info(static_cast<Category>(i), "using category ");
-        warn(static_cast<Category>(i), cat_names[i]);
-        if (i == NUM_CATEGORIES - 1)
-            info_l(static_cast<Category>(i), "");
-    }
-}
 
 int main(int argc, char** argv)
 {
@@ -46,7 +9,7 @@ int main(int argc, char** argv)
     QApplication::setApplicationName("DobieStation");
     QApplication::setOrganizationDomain("https://github.com/PSI-Rockin/DobieStation");
     QApplication a(argc, argv);
-    auto window = unique_ptr<EmuWindow>(new EmuWindow());
+    auto window = std::unique_ptr<EmuWindow>(new EmuWindow());
 
     if (window->init(argc, argv))
         return 1;
